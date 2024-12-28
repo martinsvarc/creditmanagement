90ec120074fe2e04cf229e219e5f9770ea0e4ca0
martinsvarc2 <marianekjohn9@gmail.com>
2024-12-28 19:34:23 +0100

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { CreditManagement } from './components/credit-management'
+
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-white py-12">
+      <CreditManagement />
+    </div>
+  )
+}
+
