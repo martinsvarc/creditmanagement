a0ce72431341327788e842a8fc16bc2d9c2a0f2c
martinsvarc2 <marianekjohn9@gmail.com>
2024-12-28 19:34:43 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { Montserrat } from 'next/font/google'
+import './globals.css'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['500', '800'],
+  variable: '--font-montserrat',
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} font-montserrat`}>{children}</body>
+    </html>
+  )
+}
+
