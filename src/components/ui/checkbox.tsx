c2d49f9d4f979f69454957b1c8965c09dc91adf0
martinsvarc2 <marianekjohn9@gmail.com>
2024-12-28 19:41:33 +0100

    Create checkbox.tsx

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,10 @@
+import { Checkbox } from "@/components/ui/checkbox";
+
+const CheckboxExample = () => (
+  <label>
+    <Checkbox />
+    <span>Check me</span>
+  </label>
+);
+
+export default CheckboxExample;
